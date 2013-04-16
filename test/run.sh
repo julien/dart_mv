7f59eeb872bd736135cfc5cced35b9efb43ba160
Julien Castelain <jcastelain@gmail.com>
2013-04-16 14:29:20 +0300

    updated run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
+
 set -e
- 
-# unit tests
+
+#####
+# Unit Tests
+
 echo "DumpRenderTree test/index.html"
 results=`DumpRenderTree test/index.html 2>&1`
 
 echo "$results" | grep CONSOLE
+
 echo $results | grep 'unittest-suite-success' >/dev/null
+
 echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
 
-# type analysis
+#####
+# Type Analysis
+
+echo
 echo "dart_analyzer lib/*.dart"
- 
+
 results=`dart_analyzer lib/*.dart 2>&1`
+
 echo "$results"
+
 if [ -n "$results" ]; then
-  exit 1
+    exit 1
 else
-  echo "Passed analysis."
+    echo "Passed analysis."
 fi
+
 
