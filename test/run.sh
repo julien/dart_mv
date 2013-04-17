9d5ad7a496648a6cce57f331b013dc3788f63bb1
Julien Castelain <jcastelain@gmail.com>
2013-04-17 13:14:40 +0200

    trying to fix test runner

--- test/run.sh
+++ test/run.sh
@@ -10,7 +10,7 @@ results=`DumpRenderTree test/index.html 2>&1`
 
 echo "$results" | grep CONSOLE
 
-echo $results | grep 'unittest-suite-success' >/dev/null
+echo $results | grep 'unittest-suite-done' >/dev/null
 
 echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
 
